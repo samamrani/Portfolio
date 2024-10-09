4d3be6f59ae677260c423ffafc7db2c679f291d2
atmani samira <erp.atmanis@gmail.com>
2024-10-09 12:56:45 +0200

    erreur

--- src/components/InformationPerso.jsx
+++ src/components/InformationPerso.jsx
@@ -5,15 +5,14 @@ import '../styles/main.scss';
 function InformPerso() {
   return (
 
-    <div className="container info">
-       
+   <div>
         <h1>Samira ATMANI</h1>
         <p>Développeuse Web</p>
         <p>Email : erp.atmanis@gmail.com | Téléphone : +33 6 32 57 65 81</p>
         <p>LinkedIn : <a href="https://linkedin.com/in/samira-amrani">linkedin.com/in/samira-amrani</a></p>
         <p>Github : <a href="https://Github.com/samamrani">Github.com/samamrani</a></p>
  
-    </div>
+        </div>
  
       )
       }
